2eb14268ffc34387b0cc46f91374b4d21a678bee
elnur004 <lnur004@gmail.com>
2020-11-18 02:39:12 +0400

    added game logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNum = document.querySelector('.number').textContent = Math.trunc(Math.random() *10) + 1;
+let score = 20;
 
     document.querySelector('.check').addEventListener('click', function() {
         const guess = Number(document.querySelector('.guess').value);
@@ -12,9 +13,24 @@ const secretNum = document.querySelector('.number').textContent = Math.trunc(Mat
         } else if(guess === secretNum) {
              document.querySelector('.message').textContent = '🎉 Correct Number!'
         } else if (guess < secretNum) {
-             document.querySelector('.message').textContent = '📉 Lower Number!'
+            if(score > 1) {
+                document.querySelector('.message').textContent = '📉 Lower Number!'
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = '❌ You Lose!'
+                document.querySelector('.score').textContent = 0;
+            }
+             
         } else if(guess > secretNum) {
-             document.querySelector('.message').textContent = '📈 Heigher Number!'
+            if(score > 1) {
+                document.querySelector('.message').textContent = '📈 Heigher Number!'
+                score--;
+                document.querySelector('.score').textContent = score;
+            } else {
+                document.querySelector('.message').textContent = '❌ You Lose!'
+                document.querySelector('.score').textContent = 0;
+            }
         }
      })
 
