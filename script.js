b5b6ccafb1cdda52f56ffb42360cc7d0c5c45f83
elnur004 <lnur004@gmail.com>
2020-11-18 02:08:02 +0400

    added initial game logic

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const secretNum = document.querySelector('.number').textContent = Math.trunc(Math.random() *10) + 1;
+
+    document.querySelector('.check').addEventListener('click', function() {
+        const guess = Number(document.querySelector('.guess').value);
+        console.log(secretNum);
+        console.log(guess, typeof guess);
+     
+        if(!guess) {
+         document.querySelector('.message').textContent = '⛔️ No number!';
+        } else if(guess === secretNum) {
+             document.querySelector('.message').textContent = '🎉 Correct Number!'
+        } else if (guess < secretNum) {
+             document.querySelector('.message').textContent = '📉 Lower Number!'
+        } else if(guess > secretNum) {
+             document.querySelector('.message').textContent = '📈 Heigher Number!'
+        }
+     })
+
+
+
+
